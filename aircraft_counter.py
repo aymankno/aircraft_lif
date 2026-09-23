f9818ddd97f2b55da2170cf4f77410498436736c
aymankno <aymanaghel@gmail.com>
2026-09-22 22:08:56 -0400

    Added a new neuron, fixed touch/go, reorganized neurons

--- aircraft_counter.py
+++ aircraft_counter.py
@@ -30,16 +30,24 @@ def get_slope(y_old, y_new):
     delta_y = y_old - y_new
     return delta_y
 
-neuron_1a = LIF_neuron(tau=0.1, thresh=3.0, reset=0, dt=1.0/100.0)
-neuron_1b = LIF_neuron(tau=0.1, thresh=1.2, reset=0, dt=1.0/100.0)
-neuron_2 = LIF_neuron(tau=1, thresh=2, reset=0, dt=1.0/100.0)
+# neuron_1: Takeoff
+neuron_1 = LIF_neuron(tau=0.1, thresh=1.5, reset=0, dt=1.0/100.0)
+
+# neuron_2: Landing
+neuron_2 = LIF_neuron(tau=0.1, thresh=3.0, reset=0, dt=1.0/100.0)
+
+# neuron_3: touch/go LAYER 1 - descent
+neuron_3 = LIF_neuron(tau=1, thresh=0.1, reset=0, dt=1.0/100.0)
+
+# neuron_4: touch/go LAYER 2 - ascent
+neuron_4 = LIF_neuron(tau=1, thresh=0.1, reset=0, dt=1.0/100.0)
 
 takeoffs = 0
 landings = 0
 touch_gos = 0
 y_old = None
 
-#video = "video.mp4"
+video1 = "video.mp4"
 video2 = "videos/video2.mp4"
 #video_3 = "videos/video3.mp4"
 cap = cv2.VideoCapture(video2)
@@ -71,37 +79,41 @@ while True:
                 delta_y = get_slope(y_old, centroid)
                 y_old = centroid
                 cv2.rectangle(frame, top_left, bottom_right, (0, 0, 255), 20, cv2.LINE_8)
-                if -25 <= delta_y <= -0.1:
-                    if right <= 1250:
-                        if (stats[best_i, cv2.CC_STAT_TOP] + stats[best_i, cv2.CC_STAT_HEIGHT]) <= 750:
-                            _, fired_1a = neuron_1a.step(abs(delta_y))
-                            if fired_1a == True:
+                if right <= 1250:
+                    if (stats[best_i, cv2.CC_STAT_TOP] + stats[best_i, cv2.CC_STAT_HEIGHT]) <= 750:
+                        if -25 <= delta_y <= -0.1:
+                            _, fired_2 = neuron_2.step(abs(delta_y))
+                            _, fired_3 = neuron_3.step(abs(delta_y))
+                            if fired_2 == True:
                                 landings += 1
-                                print("Landing")                         
-                        else:
-                            _, _ = neuron_2.step(1)
+                                print("Landing") 
+                                neuron_2.V = neuron_2.reset                        
+
                 if 0.1 <= delta_y <= 15:
                     if right <= 1250:
                         if (stats[best_i, cv2.CC_STAT_TOP] + stats[best_i, cv2.CC_STAT_HEIGHT]) <= 750:
-                            _, fired_1b = neuron_1b.step(abs(delta_y))
-                            if fired_1b == True:
+                            _, fired_1 = neuron_1.step(abs(delta_y))
+                            _, fired_4 = neuron_4.step(abs(delta_y))
+                            if fired_1 == True:
                                 takeoffs += 1
                                 print("Takeoff")
-                                neuron_1b.V = neuron_1b.reset
-                            ### Does not work; no current videos with touch/go, will implement later.
-                            if fired_1b == False:
-                                if neuron_2.V >= 0.5:
+                                neuron_1.V = neuron_1.reset
+                            ### go-around is experimental mechanism
+                            if fired_4 == True:
+                                if neuron_3.V > 0.1:
                                     touch_gos += 1
                                     print("Touch and go")
-                                    neuron_2.V = neuron_2.reset
-                                    neuron_1b.V = neuron_1b.reset
+                                    neuron_3.V = neuron_3.reset
+                                    neuron_4.V = neuron_4.reset
 
-    if neuron_1a.V > 0:
-        neuron_1a.leak()
-    if neuron_1b.V > 0:
-        neuron_1b.leak()
+    if neuron_1.V > 0:
+        neuron_1.leak()
     if neuron_2.V > 0:
         neuron_2.leak()
+    if neuron_3.V > 0:
+        neuron_3.leak()
+    if neuron_4.V > 0:
+        neuron_4.leak()
         
     cv2.imshow("Aircraft Counter LIF", frame)
     cv2.waitKey(33)
